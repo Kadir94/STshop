0dc9f86f2f537ba75a83977332e48146184710d3
Kadir94 <kadirkocak94@yandex.com>
2020-10-13 22:13:38 +0200

    output-fixed

--- electro_shop/index_page/get_search_selenium.py
+++ electro_shop/index_page/get_search_selenium.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import re
 import time
+import itertools
 
 
 def search_selenium(elem):
@@ -12,23 +13,27 @@ def search_selenium(elem):
     search.send_keys(elem)
     search.send_keys(Keys.RETURN)
 
-    company_name = []
+    money = []
     im = []
+    links = []
+    names = []
 
-    result2 = driver.find_elements_by_class_name("IHk3ob")
-    images = driver.find_elements_by_tag_name('img')
-
-    time.sleep(2)
-
-    for c in result2:
-        text = c.text
-        rest = re.sub('^.*?\n', '', text)
-        company_name.append(rest)
-
+    images = driver.find_elements_by_class_name('TL92Hc')
+    price = driver.find_elements_by_class_name("Nr22bf")
+    link = driver.find_elements_by_class_name("hy2WroIfzrX__merchant-name")
+    name = driver.find_elements_by_class_name("xsRiS")
+    time.sleep(4)
+    for n in name:
+        text = n.text
+        names.append(text)
+    for p in price:
+        text = p.text
+        money.append(text)
+    for l in link:
+        text = l.get_attribute("href")
+        links.append(text)
     for image in images:
         img = image.get_attribute('src')
         im.append(img)
-
-    mydict = dict(zip(company_name, im))
+    mydict = zip(money, links, im, names)
     return mydict
-
